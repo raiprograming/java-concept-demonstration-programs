62e209eaad247c43fb138bccdfc1a132c5e93efb
Enzo Godoy <enzog@iadb.com>
2020-05-19 00:01:47 -0300

    Changed invalid reference s1 > s2

--- stringconversion.java
+++ stringconversion.java
@@ -1,11 +1,12 @@
-class stringconversion{
-   public static void main(String a[]){
+class StringConversion{
+   public static void main(String[] args){
+             String s1, s2;
              int i=5;
-             String s1;
+
              s1=Integer.toString(i);
-             String s2=String.valueOf(i);
-             System.out.println("after using tostring mehthod of integer class ,s1= "+s1);
-             System.out.println("after using valueof method of  string class ,s1= "+s1);
+             s2=String.valueOf(i);
+             System.out.println("After using tostring methodod of integer class ,s1= "+s1);
+             System.out.println("After using valueof method of  string class ,s2= "+s2);
 
         }
 }
